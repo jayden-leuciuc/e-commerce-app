67e851a0d75284bbfa2dda769c073c16fa3ddb94
jayden leuciuc <leuciucjayden@gmail.com>
2021-04-06 14:24:51 -0400

    Added header component with links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-const HatsPage = () => {
-  <div>
-    <h1>Hats Page</h1>
-  </div>;
-};
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      {/*Header is outside of switch, so that it is not re-rendered on page switch */}
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
