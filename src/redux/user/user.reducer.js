edc52e9ddd22d2f941518638510d13de67d80a64
Jayden leuciuc <leuciucjayden@gmail.com>
2021-05-22 11:22:20 -0400

    Redux setup with user

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+//If state is never set, use initial state value
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
