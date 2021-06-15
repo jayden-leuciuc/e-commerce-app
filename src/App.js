d110daca7cf0301b29b7adc935551bee153f0406
Jayden Leuciuc <leuciucjayden@gmail.com>
2021-06-14 20:16:28 -0400

    Introduced redux saga flow with user and db fetch

--- src/App.js
+++ src/App.js
@@ -11,17 +11,13 @@ import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.compone
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    /* this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -33,7 +29,7 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
-    });
+    }); */
   }
 
   componentWillUnmount() {
@@ -70,9 +66,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
 //Since we are only passing in the value of currentUser and dont use it, we pass in null
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,35 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
+
+//Point of sagas is to run concurrently
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+//When db is fetched, it blocks js from loading, but takeEvery doesnt block it
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
