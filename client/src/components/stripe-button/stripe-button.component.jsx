faa66a0205fccf16bf0fcf78f8118485ade1d77a
Jayden Leuciuc <leuciucjayden@gmail.com>
2021-06-17 09:22:26 -0400

    Integrated backend for stripe test payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51J03pmBuJRZmbXGiA99UZAqpY1flBq27L6CvWfEUZF2sCyuFZ7DEGnJ8WGXBQtJP6WNnfVTcRv2ZhtDEze6YPxL100GawDk6SG';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment Successful');
+      })
+      .catch((error) => {
+        console.log('Error', JSON.parse(error));
+        alert(
+          'Issue with payment. Please use provided credit card information.'
+        );
+      });
   };
 
   return (
